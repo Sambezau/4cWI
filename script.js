129d58e1548cbe394c4c46f8365342561012835c
Samuel <samuel.meusburger@student.htldornbirn.at>
2023-11-21 09:34:34 +0100

    commit

---
+++ script.js
@@ -0,0 +1,46 @@
+document.getElementById("showTable").addEventListener("click", function(){
+    var element = document.getElementById("overlay");
+    element.style.display = "block"
+    console.log(element);
+})
+
+
+function loadMatches(){
+
+    fetch('https://api.openligadb.de/getbltable/laliga1/2023')
+      .then(response => response.json())
+      .then(json => fillTable(json))
+}
+
+
+function fillTable(data){
+    console.log(data);
+    let table = "";
+    data.forEach((team, index) => {
+        // let position = index +1;
+       let output = 
+       '<div class="component">' + team.teamName + 
+       '<div class="wappen"><img src="' + team.teamIconUrl + '" alt="" height="35px" width="35px">' + 
+       '</div><div class="pk">' + team.points + '</div> <div class="Games">' + team.matches + '</div> <div class="Sieg">' + team.won + '</div>' + 
+       '<div class="Unent">' + team.draw + '</div> <div class="Nied">' + team.lost + '</div> </div>';
+
+       table += output;
+    });
+    document.getElementById("table").innerHTML=table;
+}
+
+loadMatches();
+
+/*
+
+<div class="component">FC Barcelona
+                <div class="wappen"><img src="Barca.png" alt="" height="35px" width="35px"></div>
+                <div class="pk">20</div>
+                <div class="Games">6</div>
+                <div class="Sieg">6</div>
+                <div class="Unent">2</div>
+                <div class="Nied">0</div>
+
+            </div>
+
+*/
